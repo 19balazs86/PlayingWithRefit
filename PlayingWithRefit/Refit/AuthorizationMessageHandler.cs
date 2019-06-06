89a65757dc2b92343059b2e9e23a69cc85c19a95
Balázs Keresztúri <19balazs86@gmail.com>
2019-06-06 20:33:35 +0100

    Add link as an example for AuthorizationMessageHandler.

--- PlayingWithRefit/Refit/AuthorizationMessageHandler.cs
+++ PlayingWithRefit/Refit/AuthorizationMessageHandler.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace PlayingWithRefit.Refit
 {
+  // Example for this handler: https://github.com/reactiveui/refit#authorization-dynamic-headers-redux
   public class AuthorizationMessageHandler : DelegatingHandler
   {
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancelToken)
     {
       HttpRequestHeaders headers = request.Headers;
 
-      // If you have the following attribute in your interface, which you defined,
-      // authorization header will be "Bearer", not null.
+      // If you have the following attribute in your interface, the authorization header will be "Bearer", not null.
       // [Headers("Authorization: Bearer")]
       AuthenticationHeaderValue authHeader = headers.Authorization;
 
